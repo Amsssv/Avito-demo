1137c1736753d4a96d09bf3d4dda65d1258d7261
Andrei Moskalev <andrei.moskalev@alasco.de>
2022-04-24 20:53:05 +0200

    finish redux implementation

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,42 +1,80 @@
 import * as React from 'react';
-import { Box, } from '@mui/material';
+import {Box} from '@mui/material';
 
-import {createStore, Provider, useReduxDispatch, useReduxState} from "../provider";
-import {useReducer} from "react";
+import {connect, createStore, Provider, useDispatch, useSelector} from "../redux";
 
-const Test = () => {
-    const state = useReduxState();
-    const dispatch = useReduxDispatch();
-
-	return (
-		<div>
-			<span>{state.value}</span>
-			<button onClick={()=>dispatch({type: "INCREMENT"})}>increment</button>
-			<button onClick={()=>dispatch({type: "DECREMENT"})}>decrement</button>
-		</div>
-	)
-}
-
-const reducer = (state = {value: 0}, action) => {
+const reducer = (state = {valueOne: 0, valueTwo: 0}, action) => {
     switch (action.type) {
-        case 'INCREMENT':
-            return {value: state.value + 1}
-        case 'DECREMENT':
-            return {value: state.value - 1}
+        case 'INCREMENT_ONE':
+            return {
+                ...state,
+                valueOne: state.valueOne + 1
+            }
+        case 'DECREMENT_ONE':
+            return {
+                ...state,
+                valueOne: state.valueOne - 1
+            }
+        case 'INCREMENT_TWO':
+            return {
+                ...state,
+                valueTwo: state.valueTwo + 1
+            }
+        case 'DECREMENT_TWO':
+            return {
+                ...state,
+                valueTwo: state.valueTwo - 1
+            }
         default:
             return state
     }
 }
+
 const middlewares = [
     store => next => action => {
         console.log(store);
-		next();
+        next();
         console.log(action);
     }
 ];
 
+const store = createStore(reducer, {valueOne: 0, valueTwo: 0}, middlewares);
 
-const store = createStore(reducer, {value: 0}, middlewares)
+const mapStateToProps = state => {
+    return {
+        state
+    };
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        dispatch
+    }
+}
+
+const TestWithConnect = connect(mapStateToProps, mapDispatchToProps)((props) => {
+    const {state, dispatch} = props
+    return (
+        <div>
+            <span>{state.valueOne}</span>
+            <button onClick={() => dispatch({type: "INCREMENT_ONE"})}>increment</button>
+            <button onClick={() => dispatch({type: "DECREMENT_ONE"})}>decrement</button>
+        </div>
+    )
+});
+
+const TestWithHooks = () => {
+    const state = useSelector();
+    const dispatch = useDispatch();
+    return (
+        <div>
+            <span>{state.valueTwo}</span>
+            <button onClick={() => dispatch({type: "INCREMENT_TWO"})}>increment</button>
+            <button onClick={() => dispatch({type: "DECREMENT_TWO"})}>decrement</button>
+        </div>
+    )
+}
+
 export default function Body() {
     return (
         <Box sx={{
@@ -44,9 +82,12 @@ export default function Body() {
             boxSizing: 'border-box'
         }}>
             <Provider store={store}>
-                <Test/>
-                {/*<LeftBar />*/}
-                {/*<List />*/}
+                <div>
+                    <TestWithConnect/>
+                </div>
+                <div>
+                    <TestWithHooks/>
+                </div>
             </Provider>
         </Box>
     );
---
+++ src/redux.js
@@ -0,0 +1,93 @@
+import React, {Component, useContext, useState} from 'react';
+
+const Context = React.createContext();
+
+const defaultReducers = () => {
+};
+
+
+export const createStore = (reducers = defaultReducers, initialState = {}, enhancer = []) => {
+    let state = initialState;
+
+    const store = {
+        get state() {
+            return state;
+        },
+
+        dispatch(action) {
+            if (enhancer.length === 0) {
+                state = reducers(state, action)
+            } else {
+                const promises = enhancer.map(middleware => new Promise(resolve => middleware(store)(() => resolve())(action)));
+
+                Promise.all(promises).then(data => {
+                    state = reducers(state, action)
+                });
+            }
+        }
+    }
+
+    return store
+}
+
+export const connect = (mapStateToProps, mapDispatchToProps) => Component => props => {
+    const store = useContext(Context);
+    const connectedProps = {
+        ...props, ...mapStateToProps(store.state), ...mapDispatchToProps(store.dispatch)
+    }
+    return <Component {...connectedProps}/>
+}
+
+export const useDispatch = () => {
+    const store = useContext(Context);
+
+    if (store === undefined) {
+        return;
+    }
+
+    return store.dispatch;
+}
+
+export const useSelector = (selectorFn) => {
+    const store = useContext(Context);
+
+    if (store === undefined) {
+        return;
+    }
+
+    if (selectorFn !== undefined) {
+        return selectorFn(store.state);
+    }
+
+    return store.state;
+}
+
+export const Provider = ({children, store = null}) => {
+    if (store === null) {
+        throw new Error("Store is undefined");
+    }
+
+    //handle subscribers into via updating state
+    const [virtualStore, setNewVirtualStore] = useState(store);
+
+    const dispatch = (...args) => {
+        store.dispatch(...args);
+        //as dispatch is async function need to wait store update
+        setTimeout(() => {
+            setNewVirtualStore(newStore);
+        }, 0);
+    }
+
+    const newStore = {
+        get state() {
+            return virtualStore.state
+        },
+        dispatch
+    }
+
+    return (
+        <Context.Provider value={newStore}>
+            {children}
+        </Context.Provider>
+    )
+}
